9f2216f70361f8ec9776410890aaa9617e8915a3
JephteyAdolphe <ja983@scarletmail.rutgers.edu>
2020-08-08 04:18:56 -0400

    kinda done

--- frontend/src/components/conditions/Conditions.js
+++ frontend/src/components/conditions/Conditions.js
@@ -6,10 +6,9 @@ const Conditions = (props) => {
                <div className="results">
                    <h2><strong>{props.responseObj.name}</strong></h2>
                    <p>Current Temp: {Math.round(props.responseObj.main.temp)}</p>
-                   <p>Min Temp: {Math.round(props.responseObj.main.temp_min)}           Max Temp: {Math.round(props.responseObj.main.temp_max)}</p>
+                   <p>Min Temp: {Math.round(props.responseObj.main.temp_min)} Max Temp: {Math.round(props.responseObj.main.temp_max)}</p>
                    <p>Humidity: {Math.round(props.responseObj.main.humidity)}%</p>
                    <p>{props.responseObj.weather[0].description}</p>
-                   <p>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
                </div>
            : null
            }
